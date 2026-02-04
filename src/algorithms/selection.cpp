0bcfe3db5f0bcb48add66ee193ba407415a5a228
Mike Medford <mike@medford.dev>
2026-02-03 20:22:27 -0600

    remove comment

--- src/algorithms/selection.cpp
+++ src/algorithms/selection.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 void selectionSort(int array[]) {
-  // for (int i = 0; i < sizeof(*array); i++) {
   for (int i = 0; i < 25; i++) {
     int lowestIndex = i;
     for (int j = i + 1; j < 25; j++) {
